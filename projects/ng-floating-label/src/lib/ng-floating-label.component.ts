c176cbe5321296a9ac53f645908349b77b440e6c
David Roussov <david.roussov@gmail.com>
2019-09-04 16:55:26 +1000

    passing on component attributes to html input

--- projects/ng-floating-label/src/lib/ng-floating-label.component.ts
+++ projects/ng-floating-label/src/lib/ng-floating-label.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewChild, ElementRef, Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'ng-floating-label',
   template: `
-    <p>
-      ng-floating-label works!
-    </p>
+    <ng-container>
+      <input
+        [(ngModel)]="ngModel"
+      />
+    </ng-container>
   `,
   styles: []
 })
 export class NgFloatingLabelComponent implements OnInit {
 
-  constructor() { }
+  private elementRef: ElementRef;
+
+  @ViewChild(NgModel, { static: true, read: ElementRef }) inputElementRef: ElementRef;
+
+  constructor(element: ElementRef) { 
+    this.elementRef = element;
+  }
 
   ngOnInit() {
+    const attributes = this.elementRef.nativeElement.attributes;
+    const inputAttributes = this.inputElementRef.nativeElement.attributes;
+
+    Array.from(attributes).forEach(attribute => {
+      if (attribute.name !== 'ngModel' && !inputAttributes.getNamedItemNS(attribute.namespaceURI, attribute.name)) {
+        this.inputElementRef.nativeElement.setAttributeNS(attribute.namespaceURI, attribute.name, attribute.value);
+      }
+    });
   }
 
 }
--- projects/ng-floating-label/src/lib/ng-floating-label.module.ts
+++ projects/ng-floating-label/src/lib/ng-floating-label.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 import { NgFloatingLabelComponent } from './ng-floating-label.component';
-
-
 
 @NgModule({
   declarations: [NgFloatingLabelComponent],
   imports: [
+    FormsModule
   ],
   exports: [NgFloatingLabelComponent]
 })
